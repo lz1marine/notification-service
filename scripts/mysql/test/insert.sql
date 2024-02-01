e4d830c89cc4b0794a7063f7b30e3f8e81e52676
marin <marin.koynov@gmail.com>
2024-02-01 06:55:23 +0200

    Add initial database scripts

---
+++ scripts/mysql/test/insert.sql
@@ -0,0 +1,8 @@
+USE `users`;
+
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('1', 'admin1', 'abcd', '{email: "test@eservice.com", phone: "+35912345678", address: "test address1"}');
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('2', 'admin2', 'abcd', '{email: "test+1@eservice.com", phone: "+35912345679", address: "test address2"}');
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('3', 'admin3', 'abcd', '{email: "test+2@eservice.com", phone: "+35912345680", address: "test address3"}');
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('4', 'admin4', 'abcd', '{email: "test+3@eservice.com", phone: "+35912345681", address: "test address4"}');
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('5', 'admin5', 'abcd', '{email: "test+4@eservice.com", phone: "+35912345682", address: "test address5"}');
+INSERT INTO `users` (`id`, `username`, `password`, `profile`) VALUES ('6', 'admin6', 'abcd', '{email: "test+5@eservice.com", phone: "+35912345683", address: "test address6", slack: "whatslack"}');
